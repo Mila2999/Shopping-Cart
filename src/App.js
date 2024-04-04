d6546d3d49906f489360202b7754cdbb21188c2b
mila2999 <mila.savonik@gmail.com>
2024-04-04 11:20:01 +0300

    Dispatching different notification states depending on the current status

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-
+import { uiActions } from './store/ui-slice';
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch('https://shopping-cart-87b34-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
+    const sendCardData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending card data!',
+        })
+      );
+      const response = await fetch(
+        'https://shopping-cart-87b34-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending card data failed!');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sending card data successfully!',
+        })
+      );
+    };
+    sendCardData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending card data failed!',
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
     <Layout>
